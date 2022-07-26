922ec3a58ad0667147ca0b7c3dbdec2d16ab3eb0
LeeSangJin <tkdwls9277@naver.com>
2022-07-26 23:24:34 +0900

    22.7.26 commit

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+// Vue.prototype.$axios = axios;
+
+axios.defaults.baseURL = "http://localhost:8080";
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
+}).$mount("#app");
+Vue.prototype.$axios = axios;
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <!--<hello-world />-->
+    <navi-bar />
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
+// import HelloWorld from "../components/HelloWorld.vue";
+import naviBar from "@/components/naviBar.vue";
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
+export default Vue.extend({
+    name: "homeView",
+
+    components: {
+        // HelloWorld,
+        naviBar,
+    },
+});
 </script>
